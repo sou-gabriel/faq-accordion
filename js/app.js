68259e1bed972c38b4576a86962c4c92d4a42f05
Gabriel Ramos Nogueira <69519457+sou-gabriel@users.noreply.github.com>
2021-03-19 22:22:45 -0300

    :sparkles: Implementa alternância de temas no card

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
+const body = document.body
 const accordion = document.querySelector('.accordion')
+const inputTheme = document.querySelector('.theme__input')
 
 accordion.addEventListener('click', event => {
   const clickedElement = event.target
@@ -8,4 +10,15 @@ accordion.addEventListener('click', event => {
 
     accordionItem.classList.toggle('accordion__item--active')
   }
+})
+
+inputTheme.addEventListener('input', (event) => {
+  const inputIsChecked = event.target.checked 
+
+  if (inputIsChecked) {
+    body.classList.add('dark')
+    return
+  }
+
+  body.classList.remove('dark')
 })
